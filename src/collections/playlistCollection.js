5dd92f27253e418051ecbc36726e1580f3c089f4
Katelyn <katelyn.aeklund@gmail.com>
2018-03-28 12:40:03 +0100

    mixed content

--- src/collections/playlistCollection.js
+++ src/collections/playlistCollection.js
@@ -2,7 +2,7 @@ var app = app || {};
 
 app.Playlist = Backbone.Collection.extend({
     model: app.Song,
-    url: 'https://localhost:3000/playlist',
+    url: 'http://localhost:3000/playlist',
     saveAll: function(){
     	_.each(this.models, function(model){
     		model.save();
--- src/models/songModel.js
+++ src/models/songModel.js
@@ -6,7 +6,7 @@ app.Song = Backbone.Model.extend({
         name: 'No title',
         artist: 'Unknown'
     },
-    urlRoot: 'https://localhost:3000/playlist',
+    urlRoot: 'http://localhost:3000/playlist',
     initialize: function(options){
     	if( _.isArray(options.image) ){
     		this.set('image', this.get('image')[2]['#text']);
